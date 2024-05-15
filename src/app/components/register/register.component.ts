65ba63ee703a809b3b79437e6a665ef703805ef9
Zuegelwagen <m.inglin@gmx.ch>
2024-05-15 17:02:02 +0200

    register component styled

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
