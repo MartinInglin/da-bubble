249a977271b237f26761f1633c636d6c27d7f795
Steffen Winter <155267553+wndi318@users.noreply.github.com>
2024-05-16 18:09:53 +0200

    added new channel dialog

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AddUserToChannelComponent } from './components/add-user-to-channel/add-user-to-channel.component';
 import { ProfileDetailViewComponent } from './components/profile-detail-view/profile-detail-view.component';
+import { NewChannelComponent } from './components/new-channel/new-channel.component';
 import { HeaderComponent } from './components/shared/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, ProfileDetailViewComponent, AddUserToChannelComponent],
+  imports: [RouterOutlet, HeaderComponent, ProfileDetailViewComponent, AddUserToChannelComponent, NewChannelComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
