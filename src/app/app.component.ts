c1ac72565e2cd5b7f6277934b184fb7ef07c59f2
Steffen Winter <155267553+wndi318@users.noreply.github.com>
2024-05-16 17:33:51 +0200

    added add suer to channel dialog

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AddUserToChannelComponent } from './components/add-user-to-channel/add-user-to-channel.component';
 import { ProfileDetailViewComponent } from './components/profile-detail-view/profile-detail-view.component';
 import { HeaderComponent } from './components/shared/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, ProfileDetailViewComponent],
+  imports: [RouterOutlet, HeaderComponent, ProfileDetailViewComponent, AddUserToChannelComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
---
+++ src/app/components/add-user-to-channel/add-user-to-channel.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-add-user-to-channel',
+  standalone: true,
+  imports: [
+    CommonModule,
+    MatCardModule,
+    MatIconModule,
+    MatButtonModule,
+    FormsModule
+  ],
+  templateUrl: './add-user-to-channel.component.html',
+  styleUrl: './add-user-to-channel.component.scss'
+})
+export class AddUserToChannelComponent {
+
+}
