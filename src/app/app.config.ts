1ee977f7f24071d6043ec18793bf7dce20bec3ee
Zuegelwagen <m.inglin@gmx.ch>
2024-05-17 13:57:56 +0200

    Firebase added

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,27 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getStorage, provideStorage } from '@angular/fire/storage';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDi_vsxF7O51XBUbMubX5scAdQMGOivevw',
+  authDomain: 'da-bubble-bb34a.firebaseapp.com',
+  projectId: 'da-bubble-bb34a',
+  storageBucket: 'da-bubble-bb34a.appspot.com',
+  messagingSenderId: '396194731081',
+  appId: '1:396194731081:web:eba0ccc11274e62e9a5b98',
+};
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimationsAsync()]
+  providers: [
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    provideFirebaseApp(() => initializeApp(firebaseConfig)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideStorage(() => getStorage()),
+  ],
 };
