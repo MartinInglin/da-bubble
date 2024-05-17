6667c7e7aa796f54f7c64bb2376553efae90502b
Tay1905 <137186217+Tay1905@users.noreply.github.com>
2024-05-18 00:51:58 +0200

    LandingPage

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
-  { path: 'login', component: LoginComponent },
+  // { path: 'login', component: LoginComponent },
+  { path: '', component: LandingPageComponent },
 ];
