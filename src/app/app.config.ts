fcf84ba240705653e58adf5d90dd54272f69a06c
Steffen Winter <155267553+wndi318@users.noreply.github.com>
2024-05-18 13:44:12 +0200

    firebase fix

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -25,6 +25,6 @@ export const appConfig: ApplicationConfig = {
       provideAuth(() => getAuth()),
       provideFirestore(() => getFirestore()),
       provideStorage(() => getStorage())
-    ]),
+    ]), importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"da-bubble-bb34a","appId":"1:396194731081:web:eba0ccc11274e62e9a5b98","storageBucket":"da-bubble-bb34a.appspot.com","apiKey":"AIzaSyDi_vsxF7O51XBUbMubX5scAdQMGOivevw","authDomain":"da-bubble-bb34a.firebaseapp.com","messagingSenderId":"396194731081"}))), importProvidersFrom(provideAuth(() => getAuth())), importProvidersFrom(provideFirestore(() => getFirestore())), importProvidersFrom(provideStorage(() => getStorage())),
   ],
 };
