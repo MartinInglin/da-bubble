b4505d6e40c3989b599ceb8df8e993a2b57aeb21
Steffen Winter <155267553+wndi318@users.noreply.github.com>
2024-05-20 13:35:06 +0200

    added current user logic to dialog

--- src/app/components/current-user/current-user.component.ts
+++ src/app/components/current-user/current-user.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { UsersService } from '../../services/firestore/users.service';
+import { User } from '../../models/user.class';
 
 @Component({
   selector: 'app-current-user',
@@ -11,11 +13,19 @@ import { MatButtonModule } from '@angular/material/button';
     CommonModule,
     MatCardModule,
     MatIconModule,
-    MatButtonModule
+    MatButtonModule,
   ],
   templateUrl: './current-user.component.html',
-  styleUrl: './current-user.component.scss'
+  styleUrls: ['./current-user.component.scss']
 })
-export class CurrentUserComponent {
+export class CurrentUserComponent implements OnInit {
+  currentUser: User | null = null;
 
+  constructor(private usersService: UsersService) {}
+
+  ngOnInit(): void {
+    this.usersService.currentUser$.subscribe(user => {
+      this.currentUser = user;
+    });
+  }
 }
