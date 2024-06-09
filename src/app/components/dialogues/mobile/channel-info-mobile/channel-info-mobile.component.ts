1c7863da93a94b9323b9759340abbf5f75b68759
Steffen Winter <155267553+wndi318@users.noreply.github.com>
2024-06-09 14:44:31 +0200

    profile detail view responsive

---
+++ src/app/components/dialogues/mobile/channel-info-mobile/channel-info-mobile.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-channel-info-mobile',
+  standalone: true,
+  imports: [],
+  templateUrl: './channel-info-mobile.component.html',
+  styleUrl: './channel-info-mobile.component.scss'
+})
+export class ChannelInfoMobileComponent {
+
+}
