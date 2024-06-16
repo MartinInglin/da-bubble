55e6a1b97300c1c835d07084ae544647478a27c7
Zuegelwagen <m.inglin@gmx.ch>
2024-06-16 17:52:03 +0200

    Clean up/documentation channels service

--- src/app/services/firestore/channels.service.ts
+++ src/app/services/firestore/channels.service.ts
@@ -1,6 +1,11 @@
 import { Injectable, inject } from '@angular/core';
 import {
+  DocumentReference,
+  DocumentSnapshot,
   Firestore,
+  QuerySnapshot,
+  Transaction,
+  WriteBatch,
   arrayUnion,
   collection,
   deleteDoc,
@@ -18,6 +23,7 @@ import { BehaviorSubject, Observable } from 'rxjs';
 import { MinimalUser } from '../../models/minimal_user.class';
 import { User } from '../../models/user.class';
 import { StorageService } from '../storage.service';
+import { MinimalChannel } from '../../models/minimal_channel.class';
 
 @Injectable({
   providedIn: 'root',
@@ -63,23 +69,48 @@ export class ChannelsService {
   }
 
   /**
-   * This function creates a new channel. It therefore needs a name, a description and the users which a minimalized. For display purposes they need an id, a name and the URL of their avatar.
+   * This function creates a new channel.
    *
-   * @param name string
-   * @param description string
-   * @param users object as MinimalUser which contains id as string, name as string an avatar URL as string.
+   * @param name string, name of the channel the user defined
+   * @param description string, channel description the user defined
+   * @param currentUser User, data of the signed in user
+   * @returns
    */
   async createChannel(
     name: string,
     description: string,
     currentUser: User
   ): Promise<Channel> {
-    const users = this.currentUserToMinimalUser(currentUser);
+    try {
+      const users = this.currentUserToMinimalUser(currentUser);
+      const channelData = this.buildChannelData(name, description, users);
+      const userIds = this.extractUserIds(users);
 
-    const channelRef = collection(this.firestore, 'channels');
-    const newDocRef = doc(channelRef);
+      await this.saveChannelData(channelData);
+      await this.addChannelToUsers(channelData.id, channelData.name, userIds);
 
-    const channelData: Channel = {
+      return channelData;
+    } catch (error) {
+      console.error('Error creating channel:', error);
+      throw new Error('Failed to create channel');
+    }
+  }
+
+  /**
+   * This function builds the channel data and returns an object of type channel, added all the information given from the user creating a new channel.
+   *
+   * @param name string, name of the channel
+   * @param description string of channels description
+   * @param users array of users of type minimal user
+   * @returns updated channel data of type channel
+   */
+  private buildChannelData(
+    name: string,
+    description: string,
+    users: MinimalUser[]
+  ): Channel {
+    const newDocRef = doc(collection(this.firestore, 'channels'));
+    return {
       id: newDocRef.id,
       name: name,
       description: description,
@@ -88,16 +119,40 @@ export class ChannelsService {
       isDirectMessage: false,
       isChannel: false,
     };
-
-    await setDoc(newDocRef, channelData);
-    const userIds = users
-      .map((user) => user.id)
-      .filter((id): id is string => !!id);
-    await this.addChannelToUsers(channelData.id, channelData.name, userIds);
-
-    return channelData;
   }
 
+  /**
+   * This function saves the channel to firestore.
+   *
+   * @param channelData data of the newly created channel of type channel
+   */
+  private async saveChannelData(channelData: Channel): Promise<void> {
+    try {
+      const channelRef = collection(this.firestore, 'channels');
+      const newDocRef = doc(channelRef, channelData.id);
+      await setDoc(newDocRef, channelData);
+    } catch (error) {
+      console.error('Error saving channel data:', error);
+      throw new Error('Failed to save channel data');
+    }
+  }
+
+  /**
+   * This function filters all the users and returns the user ID's as an array of strings.
+   *
+   * @param users array of minimal users
+   * @returns ID's of the users as strings in an array
+   */
+  private extractUserIds(users: MinimalUser[]): string[] {
+    return users.map((user) => user.id).filter((id): id is string => !!id);
+  }
+
+  /**
+   * This function returns the current user as a minimal user.
+   *
+   * @param currentUser user data as type user
+   * @returns user data as type minimal user.
+   */
   currentUserToMinimalUser(currentUser: User): MinimalUser[] {
     const users = [
       {
@@ -131,6 +186,12 @@ export class ChannelsService {
     }
   }
 
+  /**
+   * This function returns the user data by the given user ID.
+   *
+   * @param userId string
+   * @returns user data of type user
+   */
   async getUserById(userId: string): Promise<User> {
     const userDocRef = doc(this.firestore, 'users', userId);
     const userDoc = await getDoc(userDocRef);
@@ -142,6 +203,12 @@ export class ChannelsService {
     return userDoc.data() as User;
   }
 
+  /**
+   * This function returns the amount of users in the channel with the given ID.
+   *
+   * @param channelId string
+   * @returns number, amount of users in a channel
+   */
   async countUsersInChannel(channelId: string): Promise<number> {
     const channelDocRef = doc(this.firestore, 'channels', channelId);
     const channelDoc = await getDoc(channelDocRef);
@@ -168,45 +235,87 @@ export class ChannelsService {
     const userDocRef = doc(this.firestore, 'users', currentUserId);
 
     await runTransaction(this.firestore, async (transaction) => {
-      const channelDoc = await transaction.get(channelDocRef);
-      const userDoc = await transaction.get(userDocRef);
-
-      if (!channelDoc.exists()) {
-        throw new Error('Channel does not exist');
-      }
-
-      if (!userDoc.exists()) {
-        throw new Error('User does not exist');
-      }
+      const { channelDoc, userDoc } = await this.checkDocumentsExist(
+        transaction,
+        channelDocRef,
+        userDocRef
+      );
 
       const channelData = channelDoc.data() as Channel;
       const users = channelData.users || [];
-      const userIndex = this.getIndexOfUser(users, currentUserId);
-
-      if (userIndex === -1) {
-        throw new Error('User not found in channel');
-      }
-
-      users.splice(userIndex, 1);
-      transaction.update(channelDocRef, { users });
+      this.removeUserFromChannelData(users, currentUserId);
 
       if (users.length === 0) {
         transaction.delete(channelDocRef);
+      } else {
+        transaction.update(channelDocRef, { users });
       }
 
       const userData = userDoc.data();
       const channels = userData['channels'] || [];
-      const channelIndex = this.getIndexOfChannel(channels, channelId);
-
-      if (channelIndex !== -1) {
-        channels.splice(channelIndex, 1);
-        transaction.update(userDocRef, { channels });
-      }
+      this.removeChannelFromUserData(channels, channelId);
+      transaction.update(userDocRef, { channels });
     });
+  }
 
-    console.log(
-      'User removed from channel and channel deleted if no users left'
-    );
+  /**
+   * This function checks if the document for the channel exists. If it exists it returns the channel document and the user document as an object.
+   *
+   * @param transaction firebase object of type transaction to keep consistency of the process
+   * @param channelDocRef document reference of the selected channel
+   * @param userDocRef document reference of the current user
+   * @returns object that contains the documents of the channel and the user
+   */
+  private async checkDocumentsExist(
+    transaction: Transaction,
+    channelDocRef: DocumentReference,
+    userDocRef: DocumentReference
+  ) {
+    const channelDoc = await transaction.get(channelDocRef);
+    const userDoc = await transaction.get(userDocRef);
+
+    if (!channelDoc.exists()) {
+      throw new Error('Channel does not exist');
+    }
+
+    if (!userDoc.exists()) {
+      throw new Error('User does not exist');
+    }
+
+    return { channelDoc, userDoc };
+  }
+
+  /**
+   * This function takes the array users and removes the current user.
+   *
+   * @param users array of the users assigned to the channel
+   * @param currentUserId string
+   */
+  private removeUserFromChannelData(
+    users: MinimalUser[],
+    currentUserId: string
+  ): void {
+    const userIndex = this.getIndexOfUser(users, currentUserId);
+    if (userIndex === -1) {
+      throw new Error('User not found in channel');
+    }
+    users.splice(userIndex, 1);
+  }
+
+  /**
+   * This function takes the array channels which is stored on the user and removes the current channel
+   *
+   * @param channels array of channels stored on the user
+   * @param channelId string
+   */
+  private removeChannelFromUserData(
+    channels: Channel[],
+    channelId: string
+  ): void {
+    const channelIndex = this.getIndexOfChannel(channels, channelId);
+    if (channelIndex !== -1) {
+      channels.splice(channelIndex, 1);
+    }
   }
 
   /**
@@ -252,6 +361,13 @@ export class ChannelsService {
     await deleteDoc(doc(this.firestore, 'channels', channelId));
   }
 
+  /**
+   * This function updates a channel if the name or the description changes.
+   *
+   * @param channelId string
+   * @param name string, new name the user submits
+   * @param description string, new description the user submits
+   */
   async updateChannel(
     channelId: string,
     name: string,
@@ -295,7 +411,7 @@ export class ChannelsService {
       }
     } catch (error) {
       console.error('Error updating channel properties:', error);
-      throw error; // Rethrow the error for handling in the calling code
+      throw error;
     }
   }
 
@@ -327,26 +443,16 @@ export class ChannelsService {
     newNameChannel: string,
     channelId: string
   ): Promise<void> {
-    const collectionRef = collection(this.firestore, 'users');
-    const querySnapshot = await getDocs(collectionRef);
-
+    const users = await this.fetchAllUsers();
     const batch = writeBatch(this.firestore);
 
-    querySnapshot.forEach((docSnapshot) => {
-      const userData = docSnapshot.data() as User;
-      const channels = userData.channels;
-
-      let updated = false;
-
-      const updatedChannels = channels.map((channel) => {
-        if (channel.id === channelId) {
-          updated = true;
-          return { ...channel, name: newNameChannel };
-        }
-        return channel;
-      });
-
-      if (updated) {
+    users.forEach((docSnapshot) => {
+      const updatedChannels = this.updateChannelsForUser(
+        docSnapshot,
+        newNameChannel,
+        channelId
+      );
+      if (updatedChannels) {
         const userDocRef = doc(this.firestore, 'users', docSnapshot.id);
         batch.update(userDocRef, { channels: updatedChannels });
       }
@@ -355,6 +461,51 @@ export class ChannelsService {
     await batch.commit();
   }
 
+  /**
+   * This function gets the whole collection of users from firestore.
+   *
+   * @returns collection of documents stored in 'users' collection
+   */
+  private async fetchAllUsers(): Promise<QuerySnapshot> {
+    const collectionRef = collection(this.firestore, 'users');
+    return await getDocs(collectionRef);
+  }
+
+  /**
+   * This function updates the channel name to the new name and returns an array of minimal channels.
+   *
+   * @param docSnapshot document snapshot
+   * @param newNameChannel string
+   * @param channelId string
+   * @returns array of updated minimal channels
+   */
+  private updateChannelsForUser(
+    docSnapshot: DocumentSnapshot,
+    newNameChannel: string,
+    channelId: string
+  ): MinimalChannel[] | null {
+    const userData = docSnapshot.data() as User;
+    const channels = userData.channels;
+
+    let updated = false;
+
+    const updatedChannels = channels.map((channel) => {
+      if (channel.id === channelId) {
+        updated = true;
+        return { ...channel, name: newNameChannel };
+      }
+      return channel;
+    });
+
+    return updated ? updatedChannels : null;
+  }
+
+  /**
+   * This function adds a user to a channel.
+   *
+   * @param channelId string
+   * @param user user added, type minimal user
+   */
   async addSingleUserToChannel(
     channelId: string,
     user: MinimalUser
@@ -369,6 +520,23 @@ export class ChannelsService {
     const channelData = channelDoc.data() as Channel;
     const users = channelData.users || [];
 
+    this.updateUsersFirestore(users, user, channelDocRef, channelId);
+  }
+
+  /**
+   * This user stores the new channel data on firestore.
+   *
+   * @param users array of minimal users stored on the channel
+   * @param user user to be added as minimal user
+   * @param channelDocRef document reference of the channel
+   * @param channelId string
+   */
+  async updateUsersFirestore(
+    users: MinimalUser[],
+    user: MinimalUser,
+    channelDocRef: DocumentReference,
+    channelId: string
+  ) {
     if (!users.some((u: MinimalUser) => u.id === user.id)) {
       users.push(user);
       await updateDoc(channelDocRef, { users });
@@ -377,12 +545,39 @@ export class ChannelsService {
     }
   }
 
+  /**
+   * This function adds all users to a channel. It does this by using a batch to make sure all transactions finish safely.
+   *
+   * @param channelId string
+   */
   async addAllUsersToChannel(channelId: string): Promise<void> {
-    const collectionRef = collection(this.firestore, 'users');
-    const querySnapshot = await getDocs(collectionRef);
-
+    const users = await this.fetchAllUsers();
+    const channelData = await this.fetchChannelData(channelId);
     const batch = writeBatch(this.firestore);
 
+    const existingUserIds = channelData.users.map((user) => user.id);
+
+    users.forEach((docSnapshot) => {
+      const userData = docSnapshot.data() as User;
+      if (!existingUserIds.includes(userData.id)) {
+        this.addUserToChannelBatch(batch, channelData, userData, channelId);
+      }
+    });
+
+    const channelDocRef = doc(this.firestore, 'channels', channelId);
+    batch.update(channelDocRef, { users: channelData.users });
+
+    await batch.commit();
+    console.log('All users added to the channel.');
+  }
+
+  /**
+   *This function gets the data of the newly created channel.
+   *
+   * @param channelId string
+   * @returns channel data as type channel
+   */
+  private async fetchChannelData(channelId: string): Promise<Channel> {
     const channelDocRef = doc(this.firestore, 'channels', channelId);
     const channelDoc = await getDoc(channelDocRef);
 
@@ -390,32 +585,43 @@ export class ChannelsService {
       throw new Error('Channel does not exist');
     }
 
-    const channelData = channelDoc.data() as Channel;
-    const existingUserIds = channelData.users.map((user) => user.id);
-
-    querySnapshot.forEach((docSnapshot) => {
-      const userData = docSnapshot.data() as User;
-      if (!existingUserIds.includes(userData.id)) {
-        const minimalUser: MinimalUser = {
-          id: userData.id,
-          name: userData.name,
-          avatar: userData.avatar,
-          email: userData.email,
-        };
-        channelData.users.push(minimalUser);
-        const userDocRef = doc(this.firestore, 'users', userData.id);
-        batch.update(userDocRef, {
-          channels: arrayUnion({ id: channelId, name: channelData.name }),
-        });
-      }
-    });
-
-    batch.update(channelDocRef, { users: channelData.users });
-
-    await batch.commit();
-    console.log('All users added to the channel.');
+    return channelDoc.data() as Channel;
   }
 
+  /**
+   * This function creates writes the batch which is finally stored.
+   * 
+   * @param batch object of type WriteBatch
+   * @param channelData data of the channel, type channel
+   * @param userData data of the current user, type user
+   * @param channelId string
+   */
+  private addUserToChannelBatch(
+    batch: WriteBatch,
+    channelData: Channel,
+    userData: User,
+    channelId: string
+  ): void {
+    const minimalUser: MinimalUser = {
+      id: userData.id,
+      name: userData.name,
+      avatar: userData.avatar,
+      email: userData.email,
+    };
+    channelData.users.push(minimalUser);
+
+    const userDocRef = doc(this.firestore, 'users', userData.id);
+    batch.update(userDocRef, {
+      channels: arrayUnion({ id: channelId, name: channelData.name }),
+    });
+  }
+
+  /**
+   * This function finds a channel by its ID and returns the channel data.
+   * 
+   * @param channelId string
+   * @returns channel data of type channel
+   */
   async getChannelById(channelId: string): Promise<Channel | null> {
     try {
       const channelDocRef = doc(this.firestore, 'channels', channelId);
