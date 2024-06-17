a24a84de2314d020c933716af4693e230b25e8e0
Zuegelwagen <m.inglin@gmx.ch>
2024-06-17 19:02:10 +0200

    users service clean code and doc

--- src/app/services/firestore/users.service.ts
+++ src/app/services/firestore/users.service.ts
@@ -67,13 +67,36 @@ export class UsersService {
   async createUser(userId: string): Promise<void> {
     const userData = this.registrationService.getUserData();
 
-    let avatar = userData.avatar;
+    const avatar = await this.getUserAvatar(userData);
 
+    const user = this.buildUser(userId, userData, avatar);
+
+    await setDoc(doc(this.firestore, 'users', userId), user);
+  }
+
+  /**
+   * This function saves an individual profile image of the user in the firebase storage and returns the corresponding URL.
+   *
+   * @param userData data that the user input in the registration process
+   * @returns URL of image
+   */
+  async getUserAvatar(userData: any): Promise<string> {
     if (userData.avatarFile && userData.avatarFile.name !== 'empty.txt') {
-      avatar = await this.storageService.saveImageUser(userData.avatarFile);
+      return await this.storageService.saveImageUser(userData.avatarFile);
     }
+    return userData.avatar;
+  }
 
-    const user: User = {
+  /**
+   * This function returns all data of the user in an object.
+   *
+   * @param userId string
+   * @param userData object
+   * @param avatar string, might be URL or path to image
+   * @returns object of type user
+   */
+  buildUser(userId: string, userData: any, avatar: string): User {
+    return {
       id: userId,
       name: userData.name,
       email: userData.email,
@@ -85,8 +108,6 @@ export class UsersService {
       isChannel: false,
       savedEmojis: [],
     };
-
-    await setDoc(doc(this.firestore, 'users', userId), user);
   }
 
   /**
@@ -135,6 +156,9 @@ export class UsersService {
     );
   }
 
+  /**
+   * This function creates a snapshot of all users
+   */
   getAllUsers(): void {
     const collectionRef = collection(this.firestore, 'users');
 
@@ -147,6 +171,12 @@ export class UsersService {
     });
   }
 
+  /**
+   * This function adds a channel to a user. This is need to display all the channel if a user is signed in.
+   *
+   * @param userId string
+   * @param channel object as minimal channel
+   */
   async addChannelToSingleUser(
     userId: string,
     channel: MinimalChannel
@@ -161,6 +191,11 @@ export class UsersService {
     }
   }
 
+  /**
+   * This function adds a channel to all users. It is needed if a user creates a new channel and selects to add all users to it.
+   *
+   * @param channel object of type minimal channel
+   */
   async addChannelToUsers(channel: MinimalChannel): Promise<void> {
     const collectionRef = collection(this.firestore, 'users');
     const userQuery = query(collectionRef);
@@ -179,6 +214,12 @@ export class UsersService {
     await Promise.all(updatePromises);
   }
 
+  /**
+   * This function updates the user if she changes credentials such as name, email or avatar.
+   *
+   * @param currentUser object of type user
+   * @param partialUser part of the the user class
+   */
   async updateUser(
     currentUser: User,
     partialUser: Partial<User>
@@ -196,6 +237,12 @@ export class UsersService {
     }
   }
 
+  /**
+   * This function creates the paths to the collection and then calls for getting the corresponding data.
+   *
+   * @param partialUser part of the the user class
+   * @param currentUser object of type user
+   */
   createPathToCollection(partialUser: Partial<User>, currentUser: User) {
     let path: 'channels' | 'directMessages' | 'threads' | 'landingPage';
 
@@ -209,6 +256,13 @@ export class UsersService {
     this.getDataCollection(path, currentUser, partialUser);
   }
 
+  /**
+   * This function gets the data of each given path. It then starts updating the data on firestore.
+   *
+   * @param path string of type path
+   * @param currentUser object of type user
+   * @param partialUser object that contains part of the user
+   */
   async getDataCollection(
     path: string,
     currentUser: User,
@@ -220,20 +274,9 @@ export class UsersService {
     querySnapshot.forEach(async (document) => {
       const data = document.data();
 
-      if (path === 'channels') {
+      if (path === 'channels' || path === 'directMessages') {
         const users = data['users'];
-        this.updateNameAvatarChannelOrDirectMessage(
-          users,
-          currentUser,
-          partialUser,
-          document.id,
-          path
-        );
-      }
-
-      if (path === 'directMessages') {
-        const users = data['users'];
-        this.updateNameAvatarChannelOrDirectMessage(
+        this.updateNameAvatar(
           users,
           currentUser,
           partialUser,
@@ -264,7 +307,16 @@ export class UsersService {
     });
   }
 
-  async updateNameAvatarChannelOrDirectMessage(
+  /**
+   * This function updates the name and the avatar of the user. It then stores them on firebase.
+   * 
+   * @param users array of minimal users
+   * @param currentUser object of type user
+   * @param partialUser part of the user object
+   * @param documentId string
+   * @param path string of type path
+   */
+  async updateNameAvatar(
     users: MinimalUser[],
     currentUser: User,
     partialUser: Partial<User>,
@@ -286,6 +338,15 @@ export class UsersService {
     }
   }
 
+  /**
+   * This function updates all posts of a collection in firestore if the user-ID in the post and the ID of the current user are identical.
+   * 
+   * @param path string of type path
+   * @param documentId string
+   * @param posts array of partial posts
+   * @param currentUser object of type user
+   * @param partialUser part of the object user
+   */
   async updatePostInFirestore(
     path: string,
     documentId: string,
@@ -309,6 +370,15 @@ export class UsersService {
     }
   }
 
+  /**
+   * This function updates all reactions of a collection in firestore if the user-ID in the post and the ID of the current user are identical.
+   * 
+   * @param path string of type path
+   * @param documentId string
+   * @param posts array of partial posts
+   * @param currentUser object of type user
+   * @param partialUser part of the object user
+   */
   async updateReactionIFirestore(
     path: string,
     documentId: string,
@@ -336,29 +406,42 @@ export class UsersService {
     }
   }
 
+    /**
+   * This function saves the last two used emojis on the user.
+   * 
+   * @param currentUserId string
+   * @param emoji string
+   * @returns if emojis are the same
+   */
+    async saveUsedEmoji(currentUserId: string, emoji: string) {
+      const docRef = doc(this.firestore, 'users', currentUserId);
+      const document = await getDoc(docRef);
+      const docData = document.data();
+  
+      if (docData) {
+        const savedEmojis = docData['savedEmojis'];
+        if (savedEmojis[1] === emoji) {
+          return;
+        } else if (savedEmojis.length == 2) {
+          savedEmojis.splice(0, 1);
+        }
+        savedEmojis.push(emoji);
+  
+        await updateDoc(docRef, { savedEmojis: savedEmojis });
+      }
+    }
+
+  /**
+   * This function sets the current user to null. It is needed to avoid data leaks after the user signed out.
+   */
   setCurrentUserNull() {
     this.currentUserSubject.next(null);
   }
 
+  /**
+   * This function unsubscribes from the user data subscription.
+   */
   public unsubscribeFromData() {
     this.unsubscribe();
-  }
-
-  async saveUsedEmoji(currentUserId: string, emoji: string) {
-    const docRef = doc(this.firestore, 'users', currentUserId);
-    const document = await getDoc(docRef);
-    const docData = document.data();
-
-    if (docData) {
-      const savedEmojis = docData['savedEmojis'];
-      if (savedEmojis[1] === emoji) {
-        return;
-      } else if (savedEmojis.length == 2) {
-        savedEmojis.splice(0, 1);
-      }
-      savedEmojis.push(emoji);
-
-      await updateDoc(docRef, { savedEmojis: savedEmojis });
-    }
   }
 }
